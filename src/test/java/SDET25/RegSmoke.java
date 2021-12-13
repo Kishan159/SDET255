15498bdf9d3d1ebdb54c1a127b151b6fdd45833d
Admin <Admin@DESKTOP-C8F0LTH>
2021-12-13 17:40:33 +0530

    Edited Regression , smoke

---
+++ src/test/java/SDET25/RegSmoke.java
@@ -0,0 +1,18 @@
+package SDET25;
+
+import org.testng.annotations.Test;
+
+public class RegSmoke {
+@Test (groups = {"Regression","smoke"})
+	
+	public void regression() {
+		
+		System.out.println("URL");
+		System.out.println("Browser");
+		System.out.println("user_name");
+		System.out.println("password");
+		System.out.println("Smoke and Regression");
+		
+	}
+
+}
