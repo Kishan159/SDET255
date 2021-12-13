19449fd36cc40d6c41e3e7759f572025d76a1655
Admin <Admin@DESKTOP-C8F0LTH>
2021-12-13 15:23:37 +0530

    Edited regression

---
+++ src/test/java/SDET25/RegressionTest.java
@@ -0,0 +1,19 @@
+package SDET25;
+
+import org.testng.annotations.Test;
+
+public class RegressionTest {
+	
+	@Test (groups = "Regression")
+	
+	public void regression() {
+		
+		System.out.println("URL");
+		System.out.println("Browser");
+		System.out.println("user_name");
+		System.out.println("password");
+		
+	}
+	}
+
+
